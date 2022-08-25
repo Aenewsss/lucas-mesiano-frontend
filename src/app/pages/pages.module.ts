39d34bb8080b8d6518435a3b49523fb2fce6780e
Walkr <davi.speck@walkr.app>
2022-08-25 03:04:58 -0300

    insert video images

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,15 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages.routing';
 import { PagesComponent } from './pages.component';
 import { RouterModule } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     PagesComponent,
-    MainComponent,
-    AboutComponent
   ],
   imports: [
     CommonModule,
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { PagesComponent } from './pages.component';
 
 const pagesRoute: Routes = [
   { path: '', component: PagesComponent },
-  { path: 'sobre-mim', component: AboutComponent },
 ];
 
 @NgModule({
