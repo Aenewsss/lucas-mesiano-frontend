656f602084b37740cb7d7e51c4c8034594abb2ad
Aenã <aenamartinelli@gmail.com>
2023-02-02 22:40:20 -0300

    new changes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages.routing';
 import { PagesComponent } from './pages.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     PagesComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
